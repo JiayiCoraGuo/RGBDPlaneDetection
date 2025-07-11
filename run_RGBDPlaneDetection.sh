ab318df78b17a4a1e4d64428a301dea418f32ec9
Cora <jguo33@g.ucla.edu>
2025-07-11 17:16:27 +0000

    3D Cut and Paste

---
+++ run_RGBDPlaneDetection.sh
@@ -0,0 +1,12 @@
+cd /home/coraguo/RIPS25-AnalogDevices-ObjectDetection/src/3d_C_P/RGBDPlaneDetection
+target_path='/home/coraguo/RIPS25-AnalogDevices-ObjectDetection/src/3d_C_P/mmdetection3d/data/sunrgbd/sunrgbd_trainval/plane'
+for i in $(seq 1 1 10335)
+do
+  mkdir $target_path/$(printf "%06d" $i)
+  echo "Detecting plane on scene ${i}"
+  rgb=/home/coraguo/RIPS25-AnalogDevices-ObjectDetection/src/3d_C_P/mmdetection3d/data/sunrgbd/sunrgbd_trainval/image_resize_640480/$(printf "%06d" $i).jpg
+  echo $rgb
+  depth=/home/coraguo/RIPS25-AnalogDevices-ObjectDetection/src/3d_C_P/mmdetection3d/data/sunrgbd/sunrgbd_trainval/raw_depth_resize_640480/$(printf "%06d" $i).png
+  echo $depth
+  ./build/RGBDPlaneDetection -o /home/coraguo/RIPS25-AnalogDevices-ObjectDetection/src/3d_C_P/mmdetection3d/data/sunrgbd/sunrgbd_trainval/image_resize_640480/$(printf "%06d" $i).jpg /home/coraguo/RIPS25-AnalogDevices-ObjectDetection/src/3d_C_P/mmdetection3d/data/sunrgbd/sunrgbd_trainval/raw_depth_resize_640480/$(printf "%06d" $i).png /home/coraguo/RIPS25-AnalogDevices-ObjectDetection/src/3d_C_P/mmdetection3d/data/sunrgbd/sunrgbd_trainval/plane/$(printf "%06d" $i)/
+done
